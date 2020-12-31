e79ea60e7754338d4d7eb58a21d2dc3b759ff66e
AnnaBullard <annabullard89@gmail.com>
2020-12-31 15:44:11 -0800

    Demo user

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
 
 function LoginFormModal() {
+  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
   const [fromType, setFormType] = useState("login");
 
@@ -17,6 +20,13 @@ function LoginFormModal() {
         setShowModal(true);
         setFormType("signup")
         }}>Sign Up</button>
+      <button onClick={()=>{
+        return dispatch(sessionActions.login({ credential:"Demo User", password:"password" })).catch(
+          (res) => {
+            if (res.data && res.data.errors) console.log(res.data.errors);
+          }
+        );
+      }} >Demo</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           {(fromType==="login") &&
